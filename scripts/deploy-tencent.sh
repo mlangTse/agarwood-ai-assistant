8f9bc9b7e8e47db2643184e140d7cc0f8307730d
mlangTse <minglang.xie@icloud.com>
2026-06-09 17:05:47 +0800

    update rag

--- scripts/deploy-tencent.sh
+++ scripts/deploy-tencent.sh
@@ -99,8 +99,20 @@ server {
 
   client_max_body_size 25m;
 
+  location = ${NEXT_PUBLIC_BASE_PATH} {
+    proxy_pass http://127.0.0.1:${PORT};
+    proxy_http_version 1.1;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection "upgrade";
+    proxy_buffering off;
+  }
+
   location ${NEXT_PUBLIC_BASE_PATH}/ {
-    proxy_pass http://127.0.0.1:${PORT}${NEXT_PUBLIC_BASE_PATH}/;
+    proxy_pass http://127.0.0.1:${PORT};
     proxy_http_version 1.1;
     proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
